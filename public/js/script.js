0245d8b6ad0d89e00726c049a368521219d90d12
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-05-02 23:13:17 -0300

    Adicionando função do carousel

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,28 @@
 var navbar = document.querySelector(".cabecalho-nav");
 
 window.addEventListener("scroll", function() {
-  if (window.scrollY === 0) {
+  if (window.scrollY <= 0) {
     navbar.style.backgroundColor = "transparent";
   } else {
     navbar.style.backgroundColor = "#121212";
   }
 });
 
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const carouselSlide = document.querySelector(".carousel-slide");
+const carouselItems = document.querySelectorAll(".carousel-item");
+let counter = 0;
+const size = carouselItems[0].clientWidth;
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  counter--;
+  carouselSlide.style.transform = `translateX(-${size * counter}px)`;
+});
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= carouselItems.length - 1) return;
+  counter++;
+  carouselSlide.style.transform = `translateX(-${size * counter}px)`;
+});
