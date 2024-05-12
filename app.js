aaa93f5a7a9e114d62b13b1e401ae7057119af75
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-05-12 11:58:22 -0300

    alterando rotas para inicialização

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
+var academiaRouter = require("./src/routes/academia");
 /* var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
@@ -27,6 +28,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
+app.use("/academia", academiaRouter); 
 /* app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
