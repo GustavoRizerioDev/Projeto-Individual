f5f90cdfe7283d5023ba50b44e479c45199501fc
GustavoRizerioDev <gustavo.rsouza@sptech.school>
2024-05-12 11:57:24 -0300

    adicionando inserts no banco na tabela academia e treino

--- src/database/script-academia.sql
+++ src/database/script-academia.sql
@@ -10,22 +10,44 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE Academia (
-  idAcademia INT NOT NULL,
+  idAcademia INT NOT NULL auto_increment,
   nome VARCHAR(45) NULL,
   descricao VARCHAR(255) NULL,
   PRIMARY KEY (idAcademia)
 );
-INSERT INTO Academia (idAcademia, nome, descricao) VALUES (1, 'Academia XYZ', 'Academia de musculação e condicionamento físico');
 
+INSERT INTO Academia (nome, descricao) VALUES
+('Smart Fit', 'Academia com preços acessíveis e equipamentos modernos, oferecendo uma ampla gama de aulas e atividades.'),
+('Bio Ritmo', 'Rede de academias com foco em treinamento funcional e variedade de modalidades.'),
+('Bodytech', 'Academia premium com equipamentos de última geração e programas de treinamento personalizados.'),
+('Academia Gustavo Borges', 'Rede de academias com foco em natação e atividades aquáticas.'),
+('Companhia Athletica', 'Academia com diversas modalidades de aulas e treinamentos personalizados.'),
+('Academia 4Move', 'Rede de academias com foco em treinamento de alta intensidade.'),
+('Alta Energia', 'Academia com ambiente descontraído e variedade de aulas.'),
+('Academia Formula', 'Rede de academias com foco em treinamento funcional e nutrição.'),
+('Academia Contours', 'Academia voltada para o público feminino, com programas de treinamento específicos.'),
+('Blue Fit', 'Rede de academias com preços acessíveis e ambiente descontraído, oferecendo uma variedade de aulas e equipamentos.'),
+('Gaviões Fitness', 'Rede de academias com foco em atividades coletivas e motivacionais.');
 
 CREATE TABLE Treinos (
-  idTreinos INT NOT NULL,
+  idTreinos INT NOT NULL auto_increment,
   nomeTreino VARCHAR(50) NULL,
   PRIMARY KEY (idTreinos)
 );
 
+INSERT INTO Treinos (nomeTreino) VALUES
+('Peito'),
+('Tríceps'),
+('Bíceps'),
+('Costas'),
+('Pernas'),
+('Ombros'),
+('Abdômen'),
+('Glúteos');
+
+
 CREATE TABLE Formulario (
-  idFormulario INT NOT NULL,
+  idFormulario INT NOT NULL auto_increment,
   fkIdusuario INT NOT NULL,
   sexo VARCHAR(45) NULL,
   fkIdAcademia INT NOT NULL,
@@ -38,7 +60,7 @@ CREATE TABLE Formulario (
 );
 
 CREATE TABLE Rotina (
-    idrotina INT NOT NULL,
+    idrotina INT NOT NULL auto_increment,
     nome VARCHAR(45) NULL,
     descricao VARCHAR(45) NULL,
     fkidusuario INT NOT NULL,
@@ -47,10 +69,8 @@ CREATE TABLE Rotina (
 );
 
 select * from usuario;
+select * from academia;
+select * from treinos;
 
-SELECT idusuario, email, nome, senha FROM usuario;
 
-use aquatech;
-
-select * from usuario;
 
